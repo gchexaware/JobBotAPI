f8d32d942f306e6db2a35644bb204db7ec00bf77
ramukhsakarp <prakash.spec07@gmail.com>
2018-06-22 11:14:42 +0530

    port changed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // set up ======================================================================
 var express  = require('express');
 var app      = express(); 
-var port  	 = process.env.PORT || 3000; 
+var port  	 = process.env.PORT || 5000; 
 
 
 
